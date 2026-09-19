e87e2b5bf96d8c83f2ace46c2858d3c2ac8b4d7f
nilakshi-patel <diyap0790@gmail.com>
2026-09-20 00:05:31 +0530

    update to hamburger in the navbar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -20,7 +20,15 @@ const Navbar = () => {
             }}>
                 <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me</button>
             
-            <img src={menu} alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
+            <button
+                type="button"
+                className="mobMenu"
+                aria-label={showMenu ? 'Close navigation menu' : 'Open navigation menu'}
+                aria-expanded={showMenu}
+                onClick={() => setShowMenu(!showMenu)}
+            >
+                <img src={menu} alt="" />
+            </button>
             <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
                 <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
                 <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>About</Link>
