75910bed058afe8f01e4d1d4dbbe0ec8733ee289
nilakshi-patel <diyap0790@gmail.com>
2026-09-21 00:40:46 +0530

    update contact from

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -26,14 +26,10 @@ const Contact = () => {
         <div id='contactPage'>
             <div id="contact">
                 <h1 className="contactPageTitle">Contact Me</h1>
-                <span className="contactDesc">
-                    Please fill out the form below to discuss any work opportunities.
-                    <br />
-                    Or email me directly at <a className="emailLink" href="mailto:diyap0790@gmail.com">diyap0790@gmail.com</a>
-                </span>
+                <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
                 <form className="contactForm" ref={form} onSubmit={sendEmail}>
-                    <input type="text" className="name" placeholder='Your name' name='Your_name' />
-                    <input type="text" className="email" placeholder='Your Email' name='Your_email' />
+                    <input type="text" className="name" placeholder='Your name' name='Your_namee' />
+                    <input type="text" className="email" placeholder='Your Email' name='from_email' />
                     <textarea name="message" placeholder='Your Message' rows={5} className='msg' ></textarea>
                     <button type="submit" value="Send" className='submitBtn'>Submit</button>
                     <div className="links">
