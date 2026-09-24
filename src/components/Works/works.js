6ba9605fe7268611c663cdc388074f3ea0781c63
nilakshi-patel <diyap0790@gmail.com>
2026-09-24 21:40:29 +0530

    exchange the logo

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -7,7 +7,7 @@ const Works = () => {
             <h2 className="worksTitle">My Portfolio</h2>
             <span className="worksDesc">I take pride in writing clean, efficient, and maintainable code while paying close attention to detail. As a Frontend Developer & Java Full Stack Developer, I enjoy creating responsive user interfaces and developing reliable full-stack applications using modern technologies. I am passionate about turning ideas into meaningful digital solutions and continuously improving my skills to build applications that deliver real value.</span>
 
-            <h2 className="skillsTitle">My Skills</h2>
+            <h2 id="skill-section" className="skillsTitle">My Skills</h2>
 
             <div className="skillsContainer">
 
